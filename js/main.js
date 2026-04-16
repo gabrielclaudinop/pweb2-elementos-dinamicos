35266036007da622bb83f1251fdf75c65a24d54f
Gabriel Claudino Pimentel <claudinopimentelgabriel@gmail.com>
2026-04-16 00:09:18 -0300

    inserting the answer to the activity

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 import flags from './model/flags.js';
+
+function carregar_bandeiras() {
+    const main = document.querySelector('main')
+    for (const bandeira of flags) {
+        let div = `
+        <div class="flag col-2 my-2 text-center">
+            <img src="./imgs/flags/${bandeira.id}.png" alt="${bandeira.name}">
+            <p>${bandeira.name}</p>
+        </div>
+        `;
+    main.insertAdjacentHTML('beforeend', div);
+    }
+}
+
+carregar_bandeiras();
